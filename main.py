d9ba4b15d54b7cc68f6e154a119b7532c9d88495
skelxton <70480071+nathanlangton@users.noreply.github.com>
2024-08-21 15:29:27 +0100

    Basic User Endpoints
    Some Basic User functionality, I plan to add a login system with the password and username to allow the viewing of analytics for specific URLS owned by the user, It would need a better authentication system rather then sending a username and password though so looking into JWT tokens to be sent and decoded to find out what user is currently using the Endpoint

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from fastapi import FastAPI, Depends
 from fastapi.routing import APIRoute
-from . import url_shortner
+from . import url_shortner, user_manager
 from .database import SessionLocal, engine
 from . import models, schemas
 from uuid import UUID
@@ -23,6 +23,20 @@ def get_db():
 @app.get("/health")
 async def health_check():
     return {"message": "HEALTHY"}
+  
+# User Related Endpoints
+
+@app.post("/create-user", response_model=schemas.User)
+async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    return user_manager.create_user(db, user)
+    
+@app.get("/get-user/{user_id}", response_model=schemas.User)
+async def create_user(user_id: int, db: Session = Depends(get_db)):
+    return user_manager.get_user_by_id(db, user_id)
+    
+    
+    
+# URL Related Endpoints
     
 @app.get("/get-all", response_model=list[schemas.UrlInformation])
 async def get_all_url(db: Session = Depends(get_db)):
